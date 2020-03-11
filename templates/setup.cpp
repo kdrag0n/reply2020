270759f2d990375e04572f3045523454fa0c6386
nikochiko <kaustubh299792458@gmail.com>
2020-03-11 22:26:02 +0530

    Add C++ template

---
+++ templates/setup.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef map<string, int> msi;
+
+#define REP(i, a, b) \
+	for (int i = a; i <= b; i++)
+
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	REP(x, 1, t)
+	{
+		// Code here, replace result with variable name used for output
+
+		cout << "Case #" << x << ": " << result << "\n";
+	}
+}
